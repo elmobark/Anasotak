413270fe82c4d320e0655c763147f11b34bee8b8
mobark <mobark.k11@outlook.com>
2016-08-17 17:58:13 -0700

    ver4.73*adding-toolbar*

---
+++ app/src/main/java/apine/mentor/com/anasotak/Slide.java
@@ -0,0 +1,17 @@
+package apine.mentor.com.anasotak;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by 99 on 8/17/2016.
+ */
+public class Slide extends AppCompatActivity {
+    private ViewPager viewPager;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
+}
